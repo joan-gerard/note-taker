65a86e42050b2f7d84aecc9fd0d676caf1548183
Joan Gerard <joan.gerard@outlook.com>
2023-05-08 22:13:02 +0200

    write loader function and display data in UI

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,19 +1,23 @@
 import React from "react";
-import { Link } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 import AddNoteForm, { links as newNoteLinks } from "~/components/AddNoteForm";
+import NoteList, { links as noteListLink } from "~/components/NoteList";
 import { getStoredNotes, storeNotes } from "~/data/notes";
-import { redirect } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
 
 interface NoteRecord {
   title: string;
   content: string;
-  id?: string;
+  id: string;
 }
 
 const notes = () => {
+  const notes: NoteRecord[] = useLoaderData();
+
   return (
     <main>
       <AddNoteForm />
+      <NoteList notes={notes} />
     </main>
   );
 };
@@ -27,20 +31,37 @@ export async function action({ request }: { request: any }) {
   const noteData: NoteRecord = {
     title: formData.get("title"),
     content: formData.get("content"),
+    id: new Date().toISOString(),
   };
 
   // Add Validation
 
   const existingNotes = await getStoredNotes();
-  noteData.id = new Date().toISOString();
 
-  const updatedNotes = existingNotes.concat(noteData);
+  const updatedNotes: NoteRecord[] = existingNotes.concat(noteData);
 
   await storeNotes(updatedNotes);
 
   return redirect("/notes");
 }
 
+// triggered when a GET request when route is hit
+export async function loader() {
+  const notes: NoteRecord[] = await getStoredNotes();
+
+  return notes;
+
+  // Below: What Remix does under the hood
+
+  // return json(notes)
+
+  // OR
+
+  // return new Response(JSON.stringify(notes), {
+  //   headers: { "Content-Type": "application/json" },
+  // });
+}
+
 export function links() {
-  return [...newNoteLinks()];
+  return [...newNoteLinks(), ...noteListLink()];
 }
