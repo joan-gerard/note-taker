70f027bd5bf0167d8f839a2e18ad9473dbbb44e9
Joan Gerard <joan.gerard@outlook.com>
2023-05-08 18:39:11 +0200

    setup project

---
+++ app/routes/_index.tsx
@@ -0,0 +1,11 @@
+import type { V2_MetaFunction } from "@remix-run/node";
+
+export const meta: V2_MetaFunction = () => {
+  return [{ title: "New Remix App" }];
+};
+
+export default function Index() {
+  return (
+    <h1>Hello</h1>
+  );
+}
